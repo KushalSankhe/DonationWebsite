04389a9c4cd31049e08ca834bf99fe4dacd71b5e
kushal <kushalsankhe8087643167@gamil.com>
2021-10-15 20:07:01 +0530

    added Navbar component done with how we work page imported images making website attractive

---
+++ src/components/Navbar.js
@@ -0,0 +1,73 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <>
+        <div>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">Navbar</a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="/">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">Link</a>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="/"
+                id="navbarDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                About
+              </a>
+              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a className="dropdown-item" href="people_behind_this.html">People behind this</a></li>
+                <li><a className="dropdown-item" href="/">About Us</a></li>
+                <li><a className="dropdown-item" href="/how_we_work.html">How we work</a></li>
+                <li><hr className="dropdown-divider" /></li>
+                <li>
+                  <a className="dropdown-item" href="/what_we_offer.html">What We Offer</a>
+                </li>
+              </ul>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/book_now.html">Book Now</a>
+            </li>
+          </ul>
+          <form className="d-flex">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </div>
+    </nav>
+        </div>
+        </>
+    )
+}
+
+export default Navbar
