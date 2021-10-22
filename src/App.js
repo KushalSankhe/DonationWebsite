9c6f572a14ae8c5276438392246098a2a747a661
kushal <kushalsankhe8087643167@gamil.com>
2021-10-22 09:49:27 +0530

    solved some bugs

---
+++ src/App.js
@@ -0,0 +1,36 @@
+
+import './App.css';
+import Navbar from './components/Navbar';
+// import BookNow from './feed/BookNow';
+// import FAQs from './feed/FAQs';
+
+// import PeopleBehindThis from './feed/PeopleBehindThis';
+// import WhatWeOffer from './feed/WhatWeOffer';
+// import HowWeWork from './components/HowWeWork';
+// import ContactUs from './feed/ContactUs';
+// import DonationInfo from './components/DonationInfo';
+// import HowWeWork from './components/HowWeWork';
+// import Navbar from './components/Navbar';
+import Index from './feed/Index';
+// import PeopleBehindThis from './components/PeopleBehindThis';
+
+function App() {
+  return (
+    <>
+    <div >
+      <Navbar/>
+      {/* <BookNow/> */}
+      {/* <FAQs/> */}
+  <Index />
+  {/* <ContactUs/> */}
+  {/* <HowWeWork/> */}
+  {/* <WhatWeOffer/>
+  <DonationInfo/> */}
+
+  {/* <PeopleBehindThis/> */}
+    </div>
+    </>
+  );
+}
+
+export default App;
